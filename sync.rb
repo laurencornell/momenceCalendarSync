8cd2548b9a2f3eebbc643e0db2995038f756d5b2
laurencornell <laurencornell+git@pm.me>
2024-10-03 10:53:07 -0600

    skip "semester" types as well

--- sync.rb
+++ sync.rb
@@ -8,9 +8,10 @@ uri = URI("https://readonly-api.momence.com/host-plugins/host/#{ENV["MOMENCE_HOS
 data = Net::HTTP.get(uri)
 session_array = JSON.parse(data, object_class: OpenStruct).payload
 calendar = Calendar.new
+binding.pry
 
 session_array.each do |session|
-  next if session.type == "course"
+  next if %w[course semester].include?(session.type)
 
   calendar.create_event(session)
 end
