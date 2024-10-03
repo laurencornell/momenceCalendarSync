e7436390236aadf63f210317a9722b366cfad310
laurencornell <laurencornell+git@pm.me>
2024-10-03 00:25:11 -0600

    make host ID env var

--- sync.rb
+++ sync.rb
@@ -4,7 +4,7 @@ require 'ostruct'
 require 'pry'
 require_relative 'calendar'
 
-uri = URI('https://readonly-api.momence.com/host-plugins/host/30822/host-schedule/sessions')
+uri = URI("https://readonly-api.momence.com/host-plugins/host/#{ENV["MOMENCE_HOST_ID"]}/host-schedule/sessions")
 data = Net::HTTP.get(uri)
 session_array = JSON.parse(data, object_class: OpenStruct).payload
 calendar = Calendar.new
